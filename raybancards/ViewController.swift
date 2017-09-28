c9a4fffa850b73a4436c4da515b9735e8df1c180
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2017-09-27 22:31:26 -0500

    Initial Commit

---
+++ raybancards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raybancards
+//
+//  Created by Luis Ezcurdia on 9/27/17.
+//  Copyright © 2017 Luis Ezcurdia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
